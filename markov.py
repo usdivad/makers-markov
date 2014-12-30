5e5c287f8ac33c860577795f7f28e5a169940810
David Su <david.d.su@gmail.com>
2014-12-30 06:18:42 -0500

    more stats about dead-ends

--- markov.py
+++ markov.py
@@ -57,7 +57,12 @@ def to_probabilities(matrix):
 def chain(transition_matrix, size):
     # cur_state = list(choice(list(transition_matrix.keys())))
     cur_state = []
-    solo_transitions = 0
+    deadend_transitions = 0
+    prev_deadend = True
+    cur_deadend = []
+    longest_deadend = []
+    deadend_counts = []
+    deadends = []
     # Prevent empty begin state (for strings only '')
     valid_state = False
     while not valid_state:
@@ -67,27 +72,46 @@ def chain(transition_matrix, size):
     result_arr = cur_state[:]
     # print 'RESULT ARR: ' + str(result_arr)
     for i in xrange(size-1):
-        next_state, solo = choose_next(transition_matrix, cur_state)
-        if solo:
-            solo_transitions += 1
-        # print 'next state: ' + str(next_state)
+        next_state, is_deadend = choose_next(transition_matrix, cur_state)
+        
+        # Collecting stats about dead ends
+        if is_deadend:
+            deadend_transitions += 1
+            cur_deadend.append(next_state)
+            prev_deadend = True
+        else:
+            if len(cur_deadend) > len(longest_deadend):
+                longest_deadend = cur_deadend
+            deadend_counts.append(len(cur_deadend))
+            deadends.append(' '.join(cur_deadend))
+            cur_deadend = []
+            prev_deadend = False
+
         result_arr.append(next_state)
-        # print result_arr
         cur_state = cur_state[1:]
         cur_state.append(next_state) #update cur_state's last elm
-        print str(i) + ': cur_state is ' + str(cur_state)
+        # print str(i) + ': cur_state is ' + str(cur_state)
     
-    print '{} solo/dead-end transitions out of {} states: {}%'.format(str(solo_transitions), str(size), str(100*float(solo_transitions)/size))
+    # Stats about process
+    if len(cur_deadend) > len(longest_deadend):
+        longest_deadend = cur_deadend
+        deadend_counts.append(len(cur_deadend))
+    print 'MARKOV PROCESS STATS:'
+    print '{} dead-end transitions out of {} states: {}%'.format(str(deadend_transitions), str(size), str(100*float(deadend_transitions)/size))
+    print 'Dead-ends:\n\'' + ' ~~~ '.join(deadends) + '\''
+    print 'Dead-end counts: ' + str(deadend_counts)
+    print 'Longest consecutive dead-ends: {} ({})'.format(str(len(longest_deadend)), ' '.join(longest_deadend))
+    print ''
     return result_arr
 
 # Choose next state in transition matrix. Assumes matrix is NOT in probability-form
 def choose_next(transition_matrix, state):
-    solo_transition = False
+    deadend_transition = False
     cur_state = tuple(state)
     # print cur_state
     state_matrix = {}
     if cur_state not in transition_matrix or len(transition_matrix[cur_state]) < 1:
-        print 'WARNING: state {} is not in transition matrix'.format(str(cur_state))
+        # print 'WARNING: state {} is not in transition matrix'.format(str(cur_state))
         rand_state = choice(list(transition_matrix.keys()))
         state_matrix = transition_matrix[rand_state]
     else:
@@ -99,43 +123,49 @@ def choose_next(transition_matrix, state):
     for sk in state_matrix:
         sv = state_matrix[sk]
         if float(sv) / sum(state_matrix.values()) == 1:
-            solo_transition = True
-            print 'WARNING: state {} can only transition to \'{}\'. Try using a smaller MARKOV_ORDER'.format(str(cur_state), str(sk))
+            deadend_transition = True
+            # print 'WARNING: state {} can only transition to \'{}\'. Try using a smaller Markov order'.format(str(cur_state), str(sk))
         for i in xrange(sv):
             # print 'sk is ' + sk
             weighted_state_arr.append(sk)
     # Choose a new state
-    return (choice(weighted_state_arr), solo_transition)
+    return (choice(weighted_state_arr), deadend_transition)
 
 def format_basic(result_arr):
-    prev_word = ''
+    prev_word = 'Nothing'
     cur_word = ''
+    formatted_result_arr = []
     for i in xrange(len(result_arr)):
+        # cur_word = re.sub(r'\s+', ' ', result_arr[i])
+        # print result_arr[i] + ' vs ' + cur_word
         cur_word = result_arr[i]
-        if prev_word != '' and not re.match('\.|\?|\!', prev_word[-1]) == None:
-            cur_word_arr = list(cur_word)
-            cur_word_arr[0] = cur_word_arr[0].upper()
-            cur_word = ''.join(cur_word_arr)
-            result_arr[i] = cur_word
-        prev_word = cur_word
+        if cur_word != '':
+            if prev_word != '' and not re.match('\.|\?|\!', prev_word[-1]) == None:
+                cur_word_arr = list(cur_word)
+                if len(cur_word_arr) > 0:
+                    cur_word_arr[0] = cur_word_arr[0].upper()
+                    cur_word = ''.join(cur_word_arr)
+            formatted_result_arr.append(cur_word)
+            prev_word = cur_word
 
-    result_arr[0] = result_arr[0].title()
-    last_word = result_arr[-1]
-    if re.match('\.|\?|\!', last_word[-1]) == None:
-        result_arr[-1] = last_word + '.'
-    return result_arr
+    formatted_result_arr[0] = formatted_result_arr[0].title()
+    last_word = formatted_result_arr[-1]
+    if len(last_word) > 0 and re.match('\.|\?|\!', last_word[-1]) == None:
+        formatted_result_arr[-1] = last_word + '.'
+    return formatted_result_arr
 
 
 # TESTS
 
 # Parse input into word array and frequency dict
 filename = 'knausgaard.txt'
+# filename = 'joyce.txt'
 word_freqs = {}
 words = []
 total_words = 0
 with open(filename, 'r') as f:
     for line in f:
-        line_words = line.split(' ')
+        line_words = re.split('\s+', line)
         for word in line_words:
             total_words += 1
             words.append(word)
@@ -145,8 +175,8 @@ with open(filename, 'r') as f:
                 word_freqs[word] = 1
 
 # Generation
-SENTENCE_LENGTH = 1000
-MARKOV_ORDER = 2
+SENTENCE_LENGTH = 100
+MARKOV_ORDER = 3
 sentence = []
 
 # Generation: random
@@ -155,7 +185,7 @@ for i in xrange(SENTENCE_LENGTH):
     # print len(words)
     # print w
     sentence.append(words[w])
-format_basic(sentence)
+sentence = format_basic(sentence)
 print ''
 print 'RANDOM GENERATION:'
 print ' '.join(sentence)
@@ -170,7 +200,7 @@ matrix = transition_matrix(words, MARKOV_ORDER)
 
 # Sentence creation
 sentence = chain(matrix, SENTENCE_LENGTH)
-format_basic(sentence)
+sentence = format_basic(sentence)
 print 'MARKOV GENERATION:'
 print ' '.join(sentence)
 print ''
