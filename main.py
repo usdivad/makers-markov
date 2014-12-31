2f4a89e511a90b06d8d146fc83dcfac5c8c5386e
David Su <david.d.su@gmail.com>
2014-12-31 02:05:32 -0500

    enclosing in a loop to generate a bunch as output files

--- main.py
+++ main.py
@@ -4,8 +4,13 @@
 
 from random import randint
 import re
+from time import time
+
 import makersmarkov as mm
 import strf
+
+# Timing
+kaishi = time()
 
 # TESTS
 # Parse input into word array and frequency dict
@@ -30,83 +35,106 @@ with open(filename, 'r') as f:
 # Generation
 SENTENCE_LENGTH = 100
 MARKOV_ORDER = 2
+NUM_SENTENCES = 100
 sentence = []
+original_sentences = []
+markov_sentences = []
+original_out = 'original_sentences.txt'
+markov_out = 'markov_sentences.txt'
 
-# Generation: subsequence of original 
-# Makes certain assumptions about the length of the text file
-offset = 0
-original_idx = 0
-continue_original = True
-valid_beginning = False
+for i in xrange(NUM_SENTENCES):
+    # Generation: subsequence of original 
+    # Makes certain assumptions about the length of the text file
+    offset = 0
+    original_idx = 0
+    continue_original = True
+    valid_beginning = False
 
-# Simple, no begin/end checks
-offset = randint(0, len(words)/2)
-for i in xrange(SENTENCE_LENGTH):
-    idx = offset+i
-    sentence.append(words[idx])
+    # Simple, no begin/end checks
+    offset = randint(0, len(words)/2)
+    for i in xrange(SENTENCE_LENGTH):
+        idx = offset+i
+        sentence.append(words[idx])
 
-# # With begin/end checks
-# while not valid_beginning:
-#     offset = randint(0, len(words)/2)
-#     original_idx = offset
-#     word = words[original_idx]
-#     if strf.is_string_beginning(word):
-#         valid_beginning = True
-#         # sentence.append(word)
-#         # print word + ' is a valid beginning'
+    # # With begin/end checks
+    # while not valid_beginning:
+    #     offset = randint(0, len(words)/2)
+    #     original_idx = offset
+    #     word = words[original_idx]
+    #     if strf.is_string_beginning(word):
+    #         valid_beginning = True
+    #         # sentence.append(word)
+    #         # print word + ' is a valid beginning'
 
-# while continue_original:
-#     # print 'idx: {}, offset: {}, max: {}'.format(str(original_idx), str(offset), str(offset+SENTENCE_LENGTH))
-#     word = words[original_idx]
-#     if word != '' and word != ' ':
-#         sentence.append(word)
-#     original_idx += 1
-#     if original_idx >= offset+SENTENCE_LENGTH:
-#         try:
-#             if strf.is_string_end(word[-1]):
-#                 # print word + ' is ending'
-#                 # print 'DONE'
-#                 continue_original = False
-#             # else:
-#             #     print word + ' is not a phrase end'
-#         except:
-#             print 'ran to the end of the file!'
-#             break
-#     # original_idx += 1
+    # while continue_original:
+    #     # print 'idx: {}, offset: {}, max: {}'.format(str(original_idx), str(offset), str(offset+SENTENCE_LENGTH))
+    #     word = words[original_idx]
+    #     if word != '' and word != ' ':
+    #         sentence.append(word)
+    #     original_idx += 1
+    #     if original_idx >= offset+SENTENCE_LENGTH:
+    #         try:
+    #             if strf.is_string_end(word[-1]):
+    #                 # print word + ' is ending'
+    #                 # print 'DONE'
+    #                 continue_original = False
+    #             # else:
+    #             #     print word + ' is not a phrase end'
+    #         except:
+    #             print 'ran to the end of the file!'
+    #             break
+    #     # original_idx += 1
 
-# sentence = strf.format_text(sentence)
-print ''
-print 'SUBSEQUENCE FROM ORIGINAL:'
-print ' '.join(sentence)
-print ''
+    # sentence = strf.format_text(sentence)
 
-# Generation: random
-# sentence = []
-# for i in xrange(SENTENCE_LENGTH):
-#     w = randint(0, len(words)-1)
-#     # print len(words)
-#     # print w
-#     sentence.append(words[w])
-# # sentence = strf.format_text(sentence)
-# print ''
-# print 'RANDOM GENERATION:'
-# print ' '.join(sentence)
-# print ''
+    original_sentences.append(' '.join(sentence))
 
-# Generation: markov
-sentence = []
-matrix = mm.transition_matrix(words, MARKOV_ORDER)
-# # Print the transition matrix
-# for k in mm.to_probabilities(matrix):
-#     # if len(matrix[k]) > 0:
-#     print '{}: {}'.format(str(k), str(matrix[k]))
+    # print ''
+    # print 'SUBSEQUENCE FROM ORIGINAL:'
+    # print ' '.join(sentence)
+    # print ''
 
-# Sentence creation
-# sentence = mm.chain(matrix, SENTENCE_LENGTH, is_sequence_beginning=strf.is_string_beginning, is_sequence_end=strf.is_string_end)
-sentence = mm.chain(matrix, SENTENCE_LENGTH)
-# sentence = strf.format_text(sentence)
-# sentence = strf.format_bible(sentence)
-# sentence = strf.format_script(sentence)
-print 'MARKOV GENERATION:'
-print ' '.join(sentence)
-print ''
+    # Generation: random
+    # sentence = []
+    # for i in xrange(SENTENCE_LENGTH):
+    #     w = randint(0, len(words)-1)
+    #     # print len(words)
+    #     # print w
+    #     sentence.append(words[w])
+    # # sentence = strf.format_text(sentence)
+    # print ''
+    # print 'RANDOM GENERATION:'
+    # print ' '.join(sentence)
+    # print ''
+
+    # Generation: markov
+    sentence = []
+    matrix = mm.transition_matrix(words, MARKOV_ORDER)
+    # # Print the transition matrix
+    # for k in mm.to_probabilities(matrix):
+    #     # if len(matrix[k]) > 0:
+    #     print '{}: {}'.format(str(k), str(matrix[k]))
+
+    # Sentence creation
+    # sentence = mm.chain(matrix, SENTENCE_LENGTH, is_sequence_beginning=strf.is_string_beginning, is_sequence_end=strf.is_string_end)
+    sentence = mm.chain(matrix, SENTENCE_LENGTH)
+    # sentence = strf.format_text(sentence)
+    # sentence = strf.format_bible(sentence)
+    # sentence = strf.format_script(sentence)
+
+    markov_sentences.append(' '.join(sentence))
+
+    # print 'MARKOV GENERATION:'
+    # print ' '.join(sentence)
+    # print ''
+
+    # print str(i) + 'th sequence done'
+
+with open(original_out, 'w') as f:
+    f.write(str(original_sentences))
+with open(markov_out, 'w') as f:
+    f.write(str(markov_sentences))
+
+
+    jiesu = time()
+    print 'Done in {} seconds'.format(str(jiesu-kaishi))
